2e211c6c9fcf260665afa638f893518844e06703
vitorhaag <126922436+vitorhaag@users.noreply.github.com>
2023-10-12 18:52:55 -0300

    codigo melhor organizado

--- imc.php
+++ imc.php
@@ -23,21 +23,13 @@
         $multiplicar = $altura * $altura;
         $imc = $peso/$multiplicar ;
         echo "<p> seu IMC é $imc</p>";
-        if ($imc <= 16.9) {
-            echo "<p>Voce está muito abaixo do peso</p>";
-        } else if ($imc < 18.4) {
-            echo "<p> Voce está ABAIXO DO PESO</p>";
-        } else if ($imc < 24.9) {
-            echo "<p> Voce está com seu PESO NORMAL </p>";
-        } else if ($imc < 29.9) {
-            echo "<p> voce está ACIMA DO PESO </p>";
-
-        } else if ($imc < 34.9) {
-            echo "<p> voce está com OBESIDADE GRAU 1</p>";
-        } else if ($imc < 40) {
-            echo "<p> voce está com OBESIDADE GRAU 2 </p>";
-        } else {
-            echo "<p> voce está com OBESIDADE GRAU 3 </p>";
+        if ($imc <= 16.9) {echo "<p>Voce está muito abaixo do peso</p>";
+        } else if ($imc < 18.4) {echo "<p> Voce está ABAIXO DO PESO</p>";
+        } else if ($imc < 24.9) { echo "<p> Voce está com seu PESO NORMAL </p>";
+        } else if ($imc < 29.9) {echo "<p> voce está ACIMA DO PESO </p>";
+        } else if ($imc < 34.9) {echo "<p> voce está com OBESIDADE GRAU 1</p>";
+        } else if ($imc < 40) {echo "<p> voce está com OBESIDADE GRAU 2 </p>";
+        } else {echo "<p> voce está com OBESIDADE GRAU 3 </p>";
         }
 
 
